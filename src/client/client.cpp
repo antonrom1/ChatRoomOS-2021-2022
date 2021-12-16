461cd8f700e76b61da002d4b54160f369cf6d396
BDMR21 <ben.david.malyane@ulb.be>
2021-12-16 12:10:26 +0100

    client fixing connection to server

--- src/client/client.cpp
+++ src/client/client.cpp
@@ -1,8 +1,3 @@
-#define USAGE "Usage: ./client <pseudo> <ip_serveur> <port>\n"
-
-#include "../common/error_handling.h"
-#include "../common/message.h"
-
 #include <cstdio>
 #include <unistd.h>
 #include <netinet/in.h>
@@ -13,6 +8,10 @@
 #include <netdb.h>
 #include <arpa/inet.h>
 
+#define USAGE "Usage: ./client <pseudo> <ip_serveur> <port>\n"
+#include "../common/error_handling.h"
+#include "../common/message.h"
+
 #define MAX_PORT_NUMBER UINT16_MAX
 #define NUM_PROGRAM_ARGS 3
 
@@ -21,23 +20,24 @@
 
 typedef uint16_t port_t;
 
-struct client_parameters_t{
+struct Argument{
   char * pseudo;
   char * ServerIp;
   port_t ServerPort;
 };
 
-int setup_client_socket_fd(int &client_address);
+int setup_client_socket_fd(port_t kServerPort, const char * ServerIp);
 port_t get_port_from_str(char *port_str);
-client_parameters_t parse_args(int argc, char **argv);
+Argument parse_args(int argc, char **argv);
+[[noreturn]] void handle_all_requests(int master_socket_fd, fd_set &all_sockets_fds);
 
-void setup_fd_set(port_t kServerPort,
+void setup_fd_set(const port_t kServerPort,
                   int &client_socket_fd,
                   const char * ServerIp,
-                  const char * ClientPseudo);
+                  fd_set sockets_fds);
 
 int main(int argc, char **argv) {
-  client_parameters_t arg = parse_args(argc, argv);
+  Argument arg = parse_args(argc, argv);
 
   const port_t kServerPort = arg.ServerPort;
   const char *kServerIp = arg.ServerIp;
@@ -45,37 +45,36 @@ int main(int argc, char **argv) {
 
 
   int client_socket_fd;
+  fd_set sockets_fds;
 
 
-  setup_fd_set(kServerPort, client_socket_fd, kServerIp, kClientPseudo);
-
-  return 0;
+  setup_fd_set(kServerPort, client_socket_fd, kServerIp, sockets_fds);
+  handle_all_requests(client_socket_fd, sockets_fds);
 }
 
 void setup_fd_set(const port_t kServerPort,
                   int &client_socket_fd,
                   const char * ServerIp,
-                  const char * ClientPseudo) {
-  struct hostent* host = gethostbyname (ServerIp);
-
-  struct sockaddr_in client_address;
-  bzero((char*)&client_address, sizeof(client_address));
-
-  client_address.sin_family = AF_INET;
-  client_address.sin_addr.s_addr = inet_addr(inet_ntoa (*(struct in_addr*)*host->h_addr_list));
-  client_address.sin_port = htons (kServerPort);
-
-  client_socket_fd= setup_client_socket_fd(client_socket_fd);
-
+                  fd_set sockets_fds) {
+  client_socket_fd= setup_client_socket_fd(kServerPort, ServerIp);
+  FD_ZERO(&sockets_fds);
+  FD_SET(client_socket_fd, &sockets_fds);
 }
 
-int setup_client_socket_fd(int &client_socket_fd) {
+int setup_client_socket_fd(port_t kServerPort, const char * ServerIp) {
   // create the socket
-  client_socket_fd = HANDLE_CALL_ERRORS(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP), EXIT_SOCK_ERROR);
+  int client_socket_fd = HANDLE_CALL_ERRORS(socket(AF_INET, SOCK_STREAM, IPPROTO_TCP), EXIT_SOCK_ERROR);
 
+  struct hostent* host = gethostbyname (ServerIp);
+  struct sockaddr_in server_address;
 
-  HANDLE_CALL_ERRORS(connect (client_socket_fd, (sockaddr*) &client_address,
-                                           sizeof(client_address)), EXIT_SOCK_ERROR);
+  bzero((char*)&server_address, sizeof(server_address));
+  server_address.sin_family = AF_INET;
+  server_address.sin_port = htons (kServerPort);
+  server_address.sin_addr.s_addr = inet_addr(inet_ntoa (*(struct in_addr*)*host->h_addr_list));
+
+  HANDLE_CALL_ERRORS(connect (client_socket_fd, (struct sockaddr*) &server_address,
+                              sizeof(server_address)), EXIT_OTHER_ERROR);
 
   return client_socket_fd;
 }
@@ -90,7 +89,7 @@ port_t get_port_from_str(char *port_str) {
   return static_cast<port_t>(l_port); // the conversion is safe, we checked for bounds
 }
 
-client_parameters_t parse_args(int argc, char **argv) {
+Argument parse_args(int argc, char **argv) {
 
   if (argc != NUM_PROGRAM_ARGS + 1) {
       log_err_and_exit("Invalid number of arguments", EXIT_BAD_USAGE_ERROR);
@@ -99,23 +98,8 @@ client_parameters_t parse_args(int argc, char **argv) {
   return {argv[1], argv[2], get_port_from_str(argv[3])};
 }
 
-void process_client_socket_request(int client_socket_fd) {
-  char receive_buffer[MAX_RECEIVE_BUFFER_SIZE + 1];
-  char output_buffer[MAX_RECEIVE_BUFFER_SIZE + 1];
-
-  bzero(receive_buffer, MAX_RECEIVE_BUFFER_SIZE);
-  bzero(output_buffer, MAX_RECEIVE_BUFFER_SIZE);
-
-  ssize_t num_bytes_written_to_buffer;
-  while (true) {
-      // TODO The client have to be able to listen and write simultaneously
-      printf("%s\n", receive_buffer);
-      if (receive_buffer[num_bytes_written_to_buffer - 1] == '\n') {
-          break;
-        }
-
-      bzero(receive_buffer, MAX_RECEIVE_BUFFER_SIZE);
-    }
-  snprintf(output_buffer, sizeof(output_buffer), "HTTP/1.0 200 OK\r\n\r\n<h1 style=\"color:white;font-family:helvetica\">Hello world</h1><style>html {background: black;}</style>");
-  write(client_socket_fd, output_buffer, strlen(output_buffer));
+[[noreturn]] void handle_all_requests(int master_socket_fd, fd_set &all_sockets_fds) {
+  for(;;)
+    printf ("okkay");
+  //TODO create the send/recv function with a thread
 }
